f07d8acae11533e0e2e85caf5fc2cde4e5d73d10
Student <student@mx23-hp>
2025-06-03 11:56:10 +0100

    go day 1

---
+++ data/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+	//var student1 string ="John"
+	//student1 := "John"
+	//var student1 int
+  //student1 = 2
+	//fmt.Println(student1)#
+	var name string
+	var luckyNumber int
+	var funFact string
+
+	name = "sat"
+	luckyNumber = 2
+	funFact = "I love to play football"
+	fmt.Println(name)
+	fmt.Println(luckyNumber)
+	fmt.Println(funFact)
+}
---
+++ func/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+func main() {
+	fmt.Println("My name is Yasin")
+	fmt.Println ("7")
+	fmt.Println ("I like football")
+}
